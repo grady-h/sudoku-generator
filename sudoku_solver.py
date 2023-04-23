0b8b9a9ba50a5287da7262ab0781e4152b79f9d9
grady-h <116502434+grady-h@users.noreply.github.com>
2023-04-23 19:03:22 -0400

    Add initial sudoku solver functionality

---
+++ sudoku_solver.py
@@ -0,0 +1,62 @@
+class SudokuSolver:
+    def __init__(self):
+        self.solution_list = []
+
+    def solve(self, board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for i in range(10):
+                        if self.check_guess(board, row, col, i):
+                            board[row][col] = i
+                            if self.solve(board):
+                                return True
+                            else:
+                                board[row][col] = 0
+                    return False
+        return True
+
+    def count_solutions(self, board):
+        self.solution_list = []
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    for i in range(1, 10):
+                        if self.check_guess(board, row, col, i):
+                            board[row][col] = i
+                            if self.is_filled(board):
+                                self.solution_list.append(board)
+                            else:
+                                self.count_solutions(board)
+                        board[row][col] = 0
+                    return
+
+    def check_guess(self, board, row, col, val):
+        return self.check_row(board, row, val) and self.check_col(board, col, val) and self.check_square(board, row, col, val)
+    
+    def check_row(self, board, row, val):
+        return val not in board[row]
+        
+    def check_col(self, board, col, val):
+        for i in range(9):
+            if board[i][col] == val:
+                return False
+        return True
+            
+    def check_square(self, board, row, col, val):
+        row_start = row//3 * 3
+        col_start = col//3 * 3
+        for i in range(3):
+            for j in range(3):
+                if board[row_start+i][col_start+j] == val:
+                    return False
+        return True
+
+    def is_filled(self, board):
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == 0:
+                    return False
+        return True
+    
